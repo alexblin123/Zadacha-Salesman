6a1ef624e36e611c5b563bff545a92a916ce27a0
alexblin123 <130595593+alexblin123@users.noreply.github.com>
2024-05-18 18:19:29 +0500

    Add files via upload

---
+++ Zadacha Salesman.cpp
@@ -0,0 +1,30 @@
+﻿#include "Graph.h"
+#include "GL/glut.h"
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+
+int main(int argc, char* argv[]) {
+
+	system("chcp 1251 > Null");
+	glutInit(&argc, argv);
+
+	graph = makeGraph();
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+	glutInitWindowSize(1350, 730);
+	glutCreateWindow("MyGraph");
+
+	WinW = glutGet(GLUT_WINDOW_WIDTH);
+	WinH = glutGet(GLUT_WINDOW_HEIGHT);
+
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+
+	glutMouseFunc(mouseClick);
+	glutMainLoop();
+
+	return 0;
+}
